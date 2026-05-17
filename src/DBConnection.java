07fc86e4cc641ba339f7c81e45a81f3cf7aaea73
sittejaned1 <sittejaned@gmail.com>
2026-05-17 22:48:08 +0800

    Fix comment formatting in getConnection method

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -21,7 +21,7 @@ public class DBConnection {
     // -------------------------------------------------------
     // getConnection()
     // Returns an active Connection, creating one if needed.
-    // --------------------------------------------------------
+    // -------------------------------------------------------
     public static Connection getConnection() {
         try {
             if (connection == null || connection.isClosed()) {
