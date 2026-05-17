bc37d592241a43efb84b6b84569799865f2d1913
mendozadarlene-dotcom <mendozadarlene993@gmail.com>
2026-05-17 21:55:37 +0800

    Update Main.java
    add to main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 // ============================================================
-// File: Main.java
-// Role: System Integrator / Menu Developer
-// Author: mendozadarlene-dotcom
-// Description: Entry point and full menu navigation for the
-//              Student Attendance System.
+
 // ============================================================
 
 public class Main {
